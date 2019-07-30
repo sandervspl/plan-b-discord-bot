0ccbb2d558514e442a308978c91c39a526392b6a
Sander Vispoel <sander.vispoel@labela.nl>
2019-07-30 11:48:09 +0200

    Remove dayjs code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import dotenv from 'dotenv';
-import 'dayjs/locale/en-gb';
-import dayjs from 'dayjs';
 import express from 'express';
 import DiscordBot from 'DiscordBot';
 
 dotenv.config();
-
-dayjs.locale('en-gb');
 
 const app = express();
 const PORT_DEFAULT = 5000;

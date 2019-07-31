6af94c4d1101ba3d5a19adc65cbcf063581b4388
Sander Vispoel <sander.vispoel@labela.nl>
2019-07-31 19:20:40 +0200

    Add dekis command

---
+++ src/commands/services/Dekis.ts
@@ -0,0 +1,13 @@
+import path from 'path';
+import Discord from 'discord.js';
+import Command from '../Command';
+
+export class Dekis extends Command {
+  constructor(discordClient: Discord.Client) {
+    super(discordClient, 'dekis');
+
+    this.onMessage((msg) => {
+      msg.channel.send('', { files: [path.resolve('static/images/dekis.jpg')] });
+    });
+  }
+}
--- src/commands/services/index.ts
+++ src/commands/services/index.ts
@@ -2,10 +2,12 @@ import { Ping } from './Ping';
 import { Release } from './Release';
 import { Marco } from './Marco';
 import { Sound } from './Sound';
+import { Dekis } from './Dekis';
 
 export default [
   Ping,
   Release,
   Marco,
   Sound,
+  Dekis,
 ] as const;

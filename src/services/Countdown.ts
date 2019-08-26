69ab000276ba6f94525e3dab473161c1048384b4
Sander Vispoel <sander.vispoel@labela.nl>
2019-08-26 11:24:23 +0200

    Add offset to all time diffs

--- src/services/Countdown.ts
+++ src/services/Countdown.ts
@@ -27,7 +27,7 @@ export class Countdown {
 
   private generateCronJob = (cronTime: string, unitType: moment.unitOfTime.Diff) => {
     const onTick = () => {
-      const offset = unitType === 'day' ? 1 : 0;
+      const offset = 1; // Because of milliseconds it will drop down 1 unit too low
       const diff = this.getDiff(unitType) + offset;
       const plural = diff !== 1 ? 's' : '';
 
